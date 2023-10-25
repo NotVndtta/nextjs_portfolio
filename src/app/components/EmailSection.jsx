9721aba92a5b0b5d93fe47571fa7318caad43ac1
NotVndtta <thenotalexey192@gmail.com>
2023-10-26 00:05:23 +0300

    Footer and message placeholder

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -5,8 +5,9 @@ import Image from 'next/image';
 
 const EmailSection = () => {
   return (
-    <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 '> 
-        <div className=''>
+    <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative'> 
+        {/* <div className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-y-1/2 '></div> */}
+        <div className='z-10'>
             <h5 className='text-xl font-bold text-white my-2 '> Lets Connect </h5>
             <p className='text-[#ADB7BE] mb-4 max-w-md'> 
                 If you want you can always contact me by email, I will always take the time to answer you :3
@@ -34,7 +35,7 @@ const EmailSection = () => {
                 placeholder='your_email@gmail.com'/>
             </div>
             <div className='mb-6'>
-                <label htmlFor='email' className='text-white block text-sm mb-2 font-medium '> Subject</label>
+                <label htmlFor='subject' className='text-white block text-sm mb-2 font-medium '> Subject</label>
                 <input 
                 type="text" 
                 id='subject' 
@@ -42,7 +43,18 @@ const EmailSection = () => {
                 className='bg-[#181818] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
                 placeholder='Type something...'/>
             </div>
-            <div className='mb-6 '></div>
+            <div className='mb-6 '>
+                <label htmlFor='message' 
+                className='text-white block text-sm mb-2 font-medium '> Message</label>
+                <textarea
+                name='message' 
+                id='message'  
+                className='bg-[#181818] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
+                placeholder='Write your message...'/>
+            </div>
+            <button
+                type='submit'
+                className='bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full'>Send Message</button>
             </form>
         </div>
     </section>
--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'
 import AboutSection from './components/AboutSection'
 import ProjectsSection from './components/ProjectsSection'
 import EmailSection from './components/EmailSection'
+import Footer from './components/Footer'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
@@ -14,6 +15,7 @@ export default function Home() {
      <ProjectsSection />
      <EmailSection />
      </div>
+     <Footer />
     </main>
   )
 }
